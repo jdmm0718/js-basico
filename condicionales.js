f06d6c51cb4b8110f785dd0a9e41b04c6ad3ce90
jdmm0718 <juandavidmm0718@gmail.com>
2022-04-18 18:35:20 -0500

    piedra

--- condicionales.js
+++ condicionales.js
@@ -17,3 +17,25 @@ else if ( edad > 18) {
 condition ? true : false;
 var numero = 1;
 var resultado = numero === 1 ? "si soy un uno" : "si no no soy un uno";
+
+var op1 = "Piedra";
+var op2 = "Papel";
+var op3 = "Tijera";
+
+var resultado = function(user, cpu){
+    if(user != cpu){
+        if(user === op1 && cpu === op3){
+            console.log("el usuario GANO con "+ op1)
+        }else if(user === op2 && cpu === op1){
+            console.log( "el usuario GANO con " + op2)
+        }else if(user === op3 && cpu === op2){
+            console.log("el usuario GANO con " + op3)
+        }else{
+            console.log("La CPU Gano!!")
+        }
+    }else if(user === cpu){
+        console.log("Empate")
+    }
+};
+
+resultado(op1,op3) //el usuario GANO con Piedra```
